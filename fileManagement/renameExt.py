900865b07ae7a38d920c2b77f9c9350aada4b1a1
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-02-05 15:20:58 -0500

    refactor input process: use a general getInput() function & use as callback to checkExtension() function; add conditional to rename loop: only do it to files with extension != newExt

--- fileManagement/renameExt.py
+++ fileManagement/renameExt.py
@@ -13,33 +13,29 @@ def checkExtension(newExt):
     """
     return "." + newExt if "." not in newExt else newExt
 
-def getPath():
+def getInput(prompt):
     """
-    Get path using 2 different input methods (Py 3.X vs 2.X)
+    Return user input using 2 different methods (Py 3.X vs 2.X)
     """
-    return input("Specify path: ") if sys.version_info[0] >= 3 else raw_input("Specify path: ")
+    return input("{} ".format(prompt)) if sys.version_info[0] >= 3 else raw_input("{} ".format(prompt))
 
-def getNewExt():
-    """
-    Get new ext using 2 different input methods (Py 3.X vs 2.X)
-    """
-    return checkExtension(input("New extension: ")) if sys.version_info[0] >= 3 \
-                                                    else checkExtension(raw_input("New ext: "))
 
 while True:
     print("\nRename extensions to make them uniform. This script doesn't reformat files!!!")
     print("Type 'x' to quit")
 
-    filePath = getPath()
+    filePath = getInput("Specify path:")
+    if filePath == "x":
+        break
 
-    if filePath.lower() == "x":
+    newExt = checkExtension(getInput("New extension:"))
+    if newExt == "x":
         break
 
     os.chdir(filePath)
-    newExt = getNewExt()
-
+    
     for f in os.listdir(os.getcwd()):
-        if "." in f and f != ".DS_Store":
+        if os.path.splitext(f)[1] != newExt and "." in f and f != ".DS_Store":
             fileName, fileExt = os.path.splitext(f)
             newName = fileName + newExt
 
