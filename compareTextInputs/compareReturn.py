25a2b695f0a4ed00b9736f1ef751434b90633b0a
JesenTanadi <jesentanadi@JesenTanadi.local>
2018-02-07 19:53:36 -0500

    some mods for CG sync

--- compareTextInputs/compareReturn.py
+++ compareTextInputs/compareReturn.py
@@ -16,11 +16,15 @@ import os
 
 os.chdir("/Users/jesentanadi/Desktop/") # remember to change directory
 
-with open("jpg.txt", 'rU') as rFile1: #.txt file of char set 1
+with open("TL CG from Indd.txt", 'rU') as rFile1: #.txt file of char set 1
     inputList1 = rFile1.readlines() #Returns a list
 
-with open("tif.txt", 'rU') as rFile2: #.txt file of char set 2
+with open("TL CG from RG.txt", 'rU') as rFile2: #.txt file of char set 2
     inputList2 = rFile2.readlines() #Returns a list
+
+inputList1 = [item.lower() for item in inputList1]
+inputList2 = [item.lower() for item in inputList2]
+
 
 #Writes result to file (new if file doesn't exist)
 with open("difference.txt", "w") as wFile:
@@ -30,13 +34,13 @@ with open("difference.txt", "w") as wFile:
     inputList2 = [item.replace("\n", "") for item in inputList2]
 
     #Goes through 1st list & compares with 2nd list
-    wFile.write("In 1st char set:\n")
+    wFile.write("In Indd:\n")
     for item in inputList1:
         if item not in inputList2:
             wFile.write(item + "\n")
 
     #Goes through 2nd list & compares with 1st list
-    wFile.write("\n\nIn 2nd char set:\n")
+    wFile.write("\n\nIn doc from RG:\n")
     for item in inputList2:
         if item not in inputList1:
             wFile.write(item + "\n")
